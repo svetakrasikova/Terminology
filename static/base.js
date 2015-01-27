3702c83150eb5fc9835936d597ae6af4d6c3f6a4
venyz <contact@ventsislavzhechev.eu>
2015-01-27 11:30:57 +0100

    Support for term searching.

--- static/base.js
+++ static/base.js
@@ -1,7 +1,7 @@
 $('#logoutLink') .click(logout);
 $('.sidebarTitle') .click(slideMenu);
 $('.quickAccessJobs') .click(function() {window.open('JobList.html?langID='+$('#languageAccessJobs').val()+'&prodID='+$('#productAccessJobs').val(), '_self', false);return false;});
-$('.quickAccessTerms') .click(function() {window.open('TermList.html?langID='+$('#languageAccessTerms').val()+'&prodID='+$('#productAccessTerms').val(), '_self', false);return false;});
+$('.quickAccessTerms') .click(function() {window.open('TermList.html?langID='+$('#languageAccessTerms').val()+'&prodID='+$('#productAccessTerms').val()+($('#termSearch').val() ? "&search="+encodeURIComponent($('#termSearch').val()) : ""), '_self', false);return false;});
 $('.tbxExportTerms') .click(function() {window.open('terminology.tbx?langID='+$('#tbxExportLanguage').val()+'&prodID='+$('#tbxExportProduct').val(), '_self', false);return false;});
 
 function logout(evt) {
